99cd47cd443b18cfeff1cf5b6e7a291039ee5e3a
chudaraj poudyal <crpoudyal@gmail.com>
2020-10-30 15:08:50 +0545

    description modified

--- src/Components/Banner/Banner.component.js
+++ src/Components/Banner/Banner.component.js
@@ -9,7 +9,7 @@ function Banner(){
 	</div>
 	<div className="main-text">
 		<h1>Hello, I am CR Poudyal</h1>
-		<p>"Developer" is a big tag, me CR poudyal as a Web|App Developer i am always ready for new challanges and journey of my development</p>
+		<p>"<b>Developer</b>" is a big tag, me CR poudyal as a <b>WEB Developer</b> i am always ready for new challanges and journey of my development</p>
 		<a href="#" className="portfolio-btn">See My Portfolio</a>
 	</div>
 	</div>
